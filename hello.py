9035399a5e6007089f37913bbaf47c7097e7cca3
ombnibus <jsh1016jwy@naver.com>
2019-12-20 22:54:01 +0900

    Add[minus]

--- hello.py
+++ hello.py
@@ -1,6 +1,12 @@
-class MyClass:
+class  Plus:
     a = int(input("숫자 입력"))
     b = int(input("숫자 입력"))
 
     print(a+b)
+
+class minus:
+    c = int(input("숫자입력"))
+    d = int(input("숫자입력"))
+
+    print(c-d)
 
